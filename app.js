4c9aea89df7d77a7797bacff5816dca13d7c8995
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-31 12:33:31 +0000

    app for get, post etc. methods

---
+++ app.js
@@ -0,0 +1,23 @@
+var methods = require('methods');
+var slice = Array.prototype.slice;
+var app = exports = module.exports = {};
+
+app.init = function() {
+    this.cache = {};
+    this.engines = {};
+    this.settings = {};
+
+    //for holding the application router
+    this._router = undefined;
+};
+
+mthods.forEach(function(method) {
+    app[method] = function(path) {
+        this.lazyrouter();
+
+        var route = this._router.route(path);
+
+        route[method].apply(route, slice.call(arguments,1));
+        return this;
+    }
+});
